c5ce811458ccc3fcec9c78baa6d7e409a2a6899c
Leon van Wissen <leon@vwissen.nl>
2021-07-26 16:53:33 +0200

    feat: simple test

---
+++ pya2a/test/test_parser.py
@@ -0,0 +1,21 @@
+import datetime
+
+from pya2a import DocumentCollection, Document
+
+# run from tldr
+OTR = 'test/saa_01cbd894-13ac-4799-b864-e3f4cba76f17.xml'
+
+
+def test_parser_otr(document=OTR):
+    d = Document(document)
+
+    # guid
+    assert d.source.guid == '01cbd894-13ac-4799-b864-e3f4cba76f17'
+
+    # date
+    assert d.source.IndexDateFrom == datetime.datetime(1655, 6, 10, 0, 0)
+    assert d.source.IndexDateTo == datetime.datetime(1655, 6, 10, 0, 0)
+
+    # persons
+    assert len(d.persons) == 2
+    assert d.persons[0].id == 'Person:961f6b15-e7fc-53f7-e053-b784100aa83b'
